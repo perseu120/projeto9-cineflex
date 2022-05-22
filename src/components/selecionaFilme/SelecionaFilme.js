bb18ae7dd2e32244dae9cbf9e0e66443d8ec7f18
josimar <josimar.17perseu@gmail.com>
2022-05-22 14:40:43 -0300

    criação de algumas rotas, busca na api os filmes e interaçao com os filmes

--- src/components/selecionaFilme/SelecionaFilme.js
+++ src/components/selecionaFilme/SelecionaFilme.js
@@ -1,24 +1,43 @@
 import styled from "styled-components";
 import Topo from "../topo/Topo.js";
 import Filme from "../filme/Filme.js"
+import axios from "axios";
+import { React, useState, useEffect } from "react";
 
-export default function SelecionaFilme(){
+export default function SelecionaFilme() {
 
-    return(
+
+
+    const [filme, setFilme] = useState([])
+
+
+
+    useEffect(() => {
+
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        promise.then((response) => {
+
+            setFilme(response.data);
+
+        })
+    }, [])
+
+    function exibirFilmes() {
+
+        return (
+            filme.map(filme => (
+                <Filme key={filme.id} id={filme.id} posterURL={filme.posterURL} />
+            ))
+        )
+    }
+
+
+    return (
 
         <>
             <Topo>Selecione o filme</Topo>
             <Container>
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-                <Filme />
-
+                {exibirFilmes()}
             </Container>
 
         </>
